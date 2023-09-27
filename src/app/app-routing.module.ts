04a37916cbe8d4d2d38f6c87224a564ee2c99f74
Ali Ahmad <ali.ahmed@alliedc.com>
2023-09-27 20:12:30 +0500

    Register Component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
-  { path: 'login', pathMatch: 'full', component: LoginComponent }
+  { path: 'login', pathMatch: 'full', component: LoginComponent },
+  { path: 'register', pathMatch: 'full', component: RegisterComponent }
 ];
 
 @NgModule({
